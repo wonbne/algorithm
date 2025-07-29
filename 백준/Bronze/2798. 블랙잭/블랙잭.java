70f28c7f7e35fb72a948dea47eb3e1c3ee2f61e6
Lee Wonbin <166809286+wonbne@users.noreply.github.com>
2025-07-29 11:22:24 +0900

    [Bronze II] Title: 블랙잭, Time: 116 ms, Memory: 14620 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,31 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int max = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = i+1; j<n; j++){
+                for(int k = j+1; k<n; k++){
+                    int sum = arr[i] + arr[j] + arr[k];
+                    if(max < sum && sum <= m){
+                        max = sum;
+                    }
+                }
+            }
+        }
+        bw.write(max+"\n");
+        bw.flush();
+    }
+}
