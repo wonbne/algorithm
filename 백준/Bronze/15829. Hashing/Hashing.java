0f7a955922ac82fd3b3fb7c2f304fd0a0b2988f0
Lee Wonbin <166809286+wonbne@users.noreply.github.com>
2025-08-12 19:55:58 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 15864 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -5,13 +5,17 @@
         public static void main(String[] args) throws IOException {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-            int L = Integer.parseInt(br.readLine());
+            long L = Integer.parseInt(br.readLine());
             String s = br.readLine();
-            double count = 0;
+            long r = 1;
+            long  m = 1234567891;
+            long count = 0;
             for(int i = 0; i<L; i++){
-                count += (s.charAt(i) - 'a' + 1) * Math.pow(31, i);
+                int v = s.charAt(i) - 'a' + 1;
+                count = (count + (v * r) % m) % m;
+                r = (r * 31) % m;
             }
-            bw.write((int)count + "\n");
+            bw.write(count + "\n");
             bw.flush();
         }
     }
