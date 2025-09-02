ee3212fc9f92cff688aaa39e10bbaa718fac67a4
Lee Wonbin <166809286+wonbne@users.noreply.github.com>
2025-09-03 03:01:38 +0900

    [Silver III] Title: 2×n 타일링, Time: 120 ms, Memory: 15948 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,18 @@
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+        }
+        bw.write(dp[n]+ "\n");
+        bw.flush();
+    }
+}
