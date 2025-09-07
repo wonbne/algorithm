c50db04fc9d153f76f41e6a8f6bd38726eff35b8
Lee Wonbin <166809286+wonbne@users.noreply.github.com>
2025-09-08 01:30:34 +0900

    [Silver II] Title: 최대 힙, Time: 304 ms, Memory: 26408 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,27 @@
+
+import java.io.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i = 0; i<n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if(x == 0){
+                if(pq.isEmpty()){
+                    bw.write(0 + "\n");
+                }else{
+                    bw.write(pq.poll() + "\n");
+                }
+            }
+            else{
+                pq.add(x);
+            }
+        }
+        bw.flush();
+    }
+}
