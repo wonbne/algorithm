8800853fb44cdef4cc34fb0347d17b5f20ee8596
Lee Wonbin <166809286+wonbne@users.noreply.github.com>
2026-02-08 18:30:48 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 116 ms, Memory: 15940 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		int[] arr = new int[26];
+		for(int i = 0; i<s.length(); i++) {
+			arr[s.charAt(i)-'a']++;
+		}
+		for(int count : arr) {
+			System.out.print(count + " ");
+		}
+		
+	}
+}
