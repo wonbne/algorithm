df9798541475239799bc530deda2e9c60a6b85a8
Lee Wonbin <166809286+wonbne@users.noreply.github.com>
2026-03-03 16:27:03 +0900

    [D3] Title: 순열1, Time: 441 ms, Memory: 101,088 KB -BaekjoonHub

--- SWEA/D3/9940. 순열1/순열1.java
+++ SWEA/D3/9940. 순열1/순열1.java
@@ -1,31 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
-    static int n;
-    static int[] arr;
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim());
-        for(int tc = 1; tc<=t; tc++){
-            n = Integer.parseInt(br.readLine().trim());
-            arr = new int[n+1];
-            StringTokenizer st = new StringTokenizer(br.readLine().trim());
-            for(int i = 1; i<=n; i++){
-                arr[i] = Integer.parseInt(st.nextToken());
-            }
-            Arrays.sort(arr);
-            boolean is = true;
-            for(int i = 1; i<=n; i++){
-                if(arr[i] != i){
-                    is = false;
-                    break;
-                }
-            }
-            System.out.println("#"+tc+" "+ (is?"Yes" : "No"));
-        }
-    }
+	public static void main(String[] args) throws IOException{
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine().trim());
+		for (int testCase = 1; testCase <= T; testCase++) {
+			sb.append("#").append(testCase).append(" ");
+			
+			int N = Integer.parseInt(br.readLine().trim());
+			int[] arr = new int[N];
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			Arrays.sort(arr);
+			
+			boolean isPermutation = true;
+			int num = 1;
+			for (int i = 0; i < N; i++) {
+				if (arr[i] != num) {
+					isPermutation = false;
+					break;
+				}
+				
+				num++;
+			}
+			
+			
+			sb.append(isPermutation ? "Yes" : "No").append("\n");
+		}
+		
+		System.out.print(sb);
+	}
 }
