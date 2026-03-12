bfefb567f5b2d7143a98363ad9ffc1e1a103d990
Lee Wonbin <166809286+wonbne@users.noreply.github.com>
2026-03-12 09:48:49 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 556 ms, Memory: 142180 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,43 +1,57 @@
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static int m;
-    static int[][] arr;
+    static List<Integer>[] list;
+    static int n, m;
     static boolean[] visited;
-    static int answer = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine().trim());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        arr = new int[n+1][n+1];
-        visited = new boolean[n+1];
-        for(int i = 1; i<m+1; i++){
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            arr[x][y] = arr[y][x] = 1;
+        visited = new boolean[n + 1];
+        list = new ArrayList[n + 1];
+        for (int i = 0; i <= n; i++) {
+            list[i] = new ArrayList<>();
         }
-        for(int i = 1; i<n+1; i++){
-            if(!visited[i]){
-                dfs(i);
-                answer++;
-            }
-        }
-        bw.write(answer + "\n");
-        bw.flush();
-    }
-    public static void dfs(int node){
-        visited[node] = true;
 
-        for(int i = 1; i<n+1; i++){
-            if(!visited[i] && arr[node][i] == 1){
-                dfs(i);
-            }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine().trim());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list[a].add(b);
+            list[b].add(a);
         }
+
+        int count = 0;
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (visited[i])
+                continue;
+            queue.offer(i);
+            visited[i] = true;
+            while (!queue.isEmpty()) {
+                int tmp = queue.poll();
+
+                for (int t : list[tmp]) {
+                    if (!visited[t]) {
+                        visited[t] = true;
+                        queue.offer(t);
+                    }
+                }
+
+            }
+            count++;
+        }
+        System.out.println(count);
     }
 }
