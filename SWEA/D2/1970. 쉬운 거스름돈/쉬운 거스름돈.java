e01d7a38ff406c0a999e2548c4c48ae55c991eb9
Lee Wonbin <166809286+wonbne@users.noreply.github.com>
2026-03-13 14:04:15 +0900

    [D2] Title: 쉬운 거스름돈, Time: 127 ms, Memory: 34,560 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
@@ -6,42 +6,40 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine().trim());
-        for(int tc = 1; tc<=t; tc++){
-            int n = Integer.parseInt(br.readLine().trim());
-            int[] arr = new int[8];
-            while(n>=10){
-                if(n>=50000){
-                    n-=50000;
-                    arr[0]++;
-                } else if(n>=10000){
-                    n-=10000;
-                    arr[1]++;
-                } else if(n>=5000){
-                    n-=5000;
-                    arr[2]++;
-                } else if(n>=1000){
-                    n-=1000;
-                    arr[3]++;
-                } else if(n>=500){
-                    n-=500;
-                    arr[4]++;
-                } else if(n>=100){
-                    n-=100;
-                    arr[5]++;
-                } else if(n>=50){
-                    n-=50;
-                    arr[6]++;
-                } else if(n>=10){
-                    n-=10;
-                    arr[7]++;
+        int max = 100000;
+        int[] coin = { 5000, 1000, 500, 100, 50, 10, 5, 1 };
+
+        for (int tc = 1; tc <= t; tc++) {
+
+            int n = Integer.parseInt(br.readLine().trim()) / 10;
+            int[] dp = new int[max + 1];
+            int[] last = new int[max + 1];
+            int[] count = new int[coin.length];
+            dp[0] = 0;
+            for (int i = 1; i <= max; i++) {
+                dp[i] = Integer.MAX_VALUE;
+            }
+            for(int i = 1; i<= max; i++){
+                for(int j = 0; j<coin.length; j++){
+                    if(i>=coin[j] && dp[i-coin[j]]+1 < dp[i]){
+                        dp[i] = dp[i-coin[j]]+1;
+                        last[i] = j;
+                    }
                 }
             }
-            System.out.println("#"+tc);
-            for(int a : arr){
-                System.out.print(a+" ");
+
+            int amount = n;
+            while(amount>0){
+                int idx = last[amount];
+                count[idx]++;
+                amount -= coin[idx];
+            }
+            System.out.println("#" + tc+" ");
+            for(int i = 0; i<coin.length; i++){
+                System.out.print(count[i]+" ");
             }
             System.out.println();
-        }   
+        }
     }
-    
+
 }
