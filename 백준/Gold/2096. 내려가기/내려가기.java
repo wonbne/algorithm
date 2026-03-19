ff1f14c227a601fa681d9b5e3c2397e19e7d6959
Lee Wonbin <166809286+wonbne@users.noreply.github.com>
2026-03-19 12:10:29 +0900

    [Gold V] Title: 내려가기, Time: 344 ms, Memory: 42280 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -8,45 +8,39 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine().trim());
-        int[][] arr = new int[n][3];
 
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine().trim());
-            for (int j = 0; j < 3; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+        int[] dpMax = new int[3];
+        int[] dpMin = new int[3];
 
-        int[][] dpMax = new int[n][3];
-        int[][] dpMin = new int[n][3];
-
+        StringTokenizer st = new StringTokenizer(br.readLine().trim());
         for (int i = 0; i < 3; i++) {
-            dpMax[0][i] = arr[0][i];
-            dpMin[0][i] = arr[0][i];
+            int tmp = Integer.parseInt(st.nextToken());
+            dpMax[i] = tmp;
+            dpMin[i] = tmp;
         }
 
         for (int i = 1; i < n; i++) {
-            for (int j = 0; j < 3; j++) {
-                int tmpMax = 0;
-                int tmpMin = Integer.MAX_VALUE;
-                for (int k = j-1; k <= j+1; k++) {
-                    if (k < 0 || k >= 3)
-                        continue;
-                    tmpMax = Math.max(dpMax[i-1][k],tmpMax);
-                    tmpMin = Math.min(dpMin[i-1][k],tmpMin);
-                }
-                dpMax[i][j] = tmpMax + arr[i][j];
-                dpMin[i][j] = tmpMin + arr[i][j];
-            }
+            st = new StringTokenizer(br.readLine().trim());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            int cpyMax0 = dpMax[0], cpyMax1 = dpMax[1], cpyMax2 = dpMax[2];
+            int cpyMin0 = dpMin[0], cpyMin1 = dpMin[1], cpyMin2 = dpMin[2];
+
+            dpMax[0] = Math.max(cpyMax0, cpyMax1) + a;
+            dpMax[1] = Math.max(Math.max(cpyMax0, cpyMax1), cpyMax2) + b;
+            dpMax[2] = Math.max(cpyMax1, cpyMax2) + c;
+
+            dpMin[0] = Math.min(cpyMin0, cpyMin1) + a;
+            dpMin[1] = Math.min(Math.min(cpyMin0, cpyMin1), cpyMin2) + b;
+            dpMin[2] = Math.min(cpyMin1, cpyMin2) + c;
         }
 
-        int max = 0; 
-        int min = Integer.MAX_VALUE;
-
-        for(int i = 0; i<3; i++){
-            max = Math.max(max, dpMax[n-1][i]);
-            min = Math.min(min, dpMin[n-1][i]);
-        }
+        int max = Math.max(Math.max(dpMax[0], dpMax[1]), dpMax[2]);
+        int min = Math.min(Math.min(dpMin[0], dpMin[1]), dpMin[2]);
         System.out.println(max + " " + min);
+
     }
 }
