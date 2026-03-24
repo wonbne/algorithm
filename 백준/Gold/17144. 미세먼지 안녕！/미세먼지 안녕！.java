203ec29d1edeed4c62836048ad46e3e6ea164ae3
Lee Wonbin <166809286+wonbne@users.noreply.github.com>
2026-03-24 15:24:53 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 676 ms, Memory: 163128 KB -BaekjoonHub

---
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -0,0 +1,125 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] arr;
+    static int r, c, t;
+    static int[] dx = { -1, 1, 0, 0 };
+    static int[] dy = { 0, 0, -1, 1 };
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine().trim());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        t = Integer.parseInt(st.nextToken());
+        arr = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            st = new StringTokenizer(br.readLine().trim());
+            for (int j = 0; j < c; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        bye();
+        int count = 0;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (arr[i][j] > 0) {
+                    count += arr[i][j];
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static void bye() {
+        Queue<int[]> queue = new LinkedList<>();
+        int time = 0;
+        int[][] air = new int[2][2];
+        while (time < t) {
+            int airCount = 0;
+            for (int i = 0; i < r; i++) {
+                for (int j = 0; j < c; j++) {
+                    if (arr[i][j] > 0) {
+                        queue.offer(new int[] { i, j });
+                    } else if (arr[i][j] == -1) {
+                        air[airCount][0] = i;
+                        air[airCount++][1] = j;
+                    }
+                }
+            }
+            int[][] diff = new int[r][c];
+
+            while (!queue.isEmpty()) {
+                int[] tmp = queue.poll();
+                int x = tmp[0];
+                int y = tmp[1];
+                int count = 0;
+                for (int d = 0; d < 4; d++) {
+                    int nx = x + dx[d];
+                    int ny = y + dy[d];
+
+                    if (nx >= r || ny >= c || nx < 0 || ny < 0 || arr[nx][ny] == -1)
+                        continue;
+
+                    diff[nx][ny] += arr[x][y] / 5;
+                    count++;
+                }
+                arr[x][y] -= arr[x][y] / 5 * count;
+            }
+
+            for(int i = 0; i<r; i++){
+                for(int j = 0; j<c; j++){
+                    arr[i][j] += diff[i][j];
+                }
+            }
+            for (int k = 0; k < 2; k++) {
+                int x = air[k][0];
+                int y = air[k][1];
+                if (k == 0) {
+                    // 하
+                    for (int i = x - 1; i >= 1; i--) {
+                        arr[i][y] = arr[i - 1][y];
+                    }
+                    // 좌
+                    for (int i = 0; i < c - 1; i++) {
+                        arr[0][i] = arr[0][i + 1];
+                    }
+                    // 상
+                    for (int i = 0; i < x; i++) {
+                        arr[i][c - 1] = arr[i + 1][c - 1];
+                    }
+                    // 우
+                    for (int i = c - 1; i >= 2; i--) {
+                        arr[x][i] = arr[x][i - 1];
+                    }
+                    arr[x][y + 1] = 0;
+                } else {
+                    // 상
+                    for (int i = x + 1; i < r - 1; i++) {
+                        arr[i][y] = arr[i + 1][y];
+                    }
+
+                    // 좌
+                    for (int i = 0; i < c - 1; i++) {
+                        arr[r - 1][i] = arr[r - 1][i + 1];
+                    }
+                    // 하
+                    for (int i = r - 1; i >= x + 1; i--) {
+                        arr[i][c - 1] = arr[i - 1][c - 1];
+                    }
+                    // 우
+                    for (int i = c - 1; i >= 2; i--) {
+                        arr[x][i] = arr[x][i - 1];
+                    }
+                    arr[x][y + 1] = 0;
+                }
+            }
+            time++;
+        }
+    }
+}
