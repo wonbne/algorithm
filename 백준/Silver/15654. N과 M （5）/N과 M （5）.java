663260305bf9730e70572854015e3ea0a7a631d7
Lee Wonbin <166809286+wonbne@users.noreply.github.com>
2026-04-01 15:07:57 +0900

    [Silver III] Title: N과 M (5), Time: 2444 ms, Memory: 86376 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static boolean[] visited;
+    static int[] arr;
+    static int[] result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine().trim());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        visited = new boolean[n];
+        arr = new int[n];
+        result = new int[m];
+        st = new StringTokenizer(br.readLine().trim());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        dfs(0);
+    }
+
+    public static void dfs(int count) {
+        if (count == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(result[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                result[count] = arr[i];
+                dfs(count + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+}
