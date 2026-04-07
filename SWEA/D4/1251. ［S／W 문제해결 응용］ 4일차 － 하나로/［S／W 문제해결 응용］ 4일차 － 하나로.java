ead3afd4d14766aacd2fe0037595aeef5c47e7b3
Lee Wonbin <166809286+wonbne@users.noreply.github.com>
2026-04-07 17:27:16 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 746 ms, Memory: 103,584 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -7,82 +7,62 @@ public class Solution {
     static int n;
     static double e;
     static int[] parent;
-    static List<Node> nodes;
-    static List<Edge> edges;
-
+    static boolean[] visited;
+    static double total;
+    static int[] xArr, yArr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine().trim());
         for (int tc = 1; tc <= t; tc++) {
+            total = 0;
             n = Integer.parseInt(br.readLine().trim());
             parent = new int[n+1];
+            visited = new boolean[n+1];
             for(int i = 0; i<=n; i++){
                 parent[i] = i;
             }
-            nodes = new ArrayList<>();
-            edges = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                nodes.add(new Node());
-            }
+
+            xArr = new int[n+1];
+            yArr = new int[n+1];
             StringTokenizer st = new StringTokenizer(br.readLine().trim());
-            for (int i = 0; i < n; i++) {
-                nodes.get(i).x = Integer.parseInt(st.nextToken());
+            for (int i = 1; i <= n; i++) {
+                xArr[i] = Integer.parseInt(st.nextToken());
             }
             st = new StringTokenizer(br.readLine().trim());
-            for (int i = 0; i < n; i++) {
-                nodes.get(i).y = Integer.parseInt(st.nextToken());
+            for (int i = 1; i <= n; i++) {
+                yArr[i] = Integer.parseInt(st.nextToken());
             }
             e = Double.parseDouble(br.readLine().trim());
 
-            for (int i = 0; i < n - 1; i++) {
-                for (int j = i + 1; j < n; j++) {
-                    long dx = nodes.get(i).x - nodes.get(j).x;
-                    long dy = nodes.get(i).y - nodes.get(j).y;
-                    long weight = dx * dx + dy * dy;
-                    edges.add(new Edge(i, j, weight));
 
+            prim();
+            System.out.println("#"+tc+" "+ Math.round(total));
+        }
+    }
+
+    public static void prim(){
+        PriorityQueue<long[]> queue = new PriorityQueue<>((a,b)->Long.compare(a[1],b[1]));
+        queue.add(new long[]{1,0});
+        int count = 0;
+        while(!queue.isEmpty()){
+            long[] tmp = queue.poll();
+            long start = tmp[0];
+            long weight = tmp[1];
+            if(visited[(int)start]) continue;
+            visited[(int)start] = true;
+            total += e * weight;
+            count++;
+            if(count == n) break;
+            for(int next = 1; next<= n; next++){
+                if(!visited[next]){
+                    long dx = xArr[(int)start] - xArr[next];
+                    long dy = yArr[(int)start] - yArr[next];
+                    queue.add(new long[]{next, dx*dx + dy*dy});
                 }
             }
-            Collections.sort(edges, (a, b) -> Long.compare(a.weight, b.weight));
-            double min = 0;
-            for(int i = 0; i<edges.size(); i++){
-                if(find(edges.get(i).start) != find(edges.get(i).end)){
-                    min += e * edges.get(i).weight;
-                    union(edges.get(i).start, edges.get(i).end);
-                }
-            }
-            System.out.println("#"+tc+" "+ Math.round(min));
         }
+
+
     }
 
-    static void union(int a, int b){
-        int root1 = find(a);
-        int root2 = find(b);
-
-        if(root1 != root2){
-            parent[root1] = root2;
-        }
-    }
-
-    static int find(int x){
-        if(parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
-    }
-
-    static class Node {
-        int x;
-        int y;
-    }
-
-    static class Edge {
-        int start;
-        int end;
-        long weight;
-
-        public Edge(int start, int end, long weight) {
-            this.start = start;
-            this.end = end;
-            this.weight = weight;
-        }
-    }
 }
