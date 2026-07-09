ba637ac878a10c9b9710ce74a4028687ecee62bf
Lee Wonbin <166809286+wonbne@users.noreply.github.com>
2026-07-09 13:12:55 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 49.35 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i<participant.length; i++){
+            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
+        }
+        
+        for(int i = 0; i<completion.length; i++){
+            map.put(completion[i], map.get(completion[i]) - 1);
+        }
+        
+        for(int i = 0; i<participant.length; i++){
+            if(map.get(participant[i]) > 0){
+                answer = participant[i];
+                break;
+            }
+        }
+        
+        
+        
+        return answer;
+    }
+}
